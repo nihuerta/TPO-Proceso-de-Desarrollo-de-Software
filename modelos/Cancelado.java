4ba71112c467a83a7b0abc96aa16f7fc356d6515
Federico Herrera <fede.juan.herrera@gmail.com>
2025-05-19 13:29:06 -0300

    adding EstadoPartido

---
+++ modelos/Cancelado.java
@@ -0,0 +1,19 @@
+package modelos;
+
+
+public class Cancelado implements IEstadoPartido {
+    @Override
+    public void validarCondicions(Partido partido) {
+        // TODO: Implementar la lógica para validar las condiciones del partido cancelado
+    }
+
+    @Override
+    public void aceptarPartido(Partido partido) {
+        // TODO: Implementar la lógica para aceptar el partido
+    }
+
+    @Override
+    public void rechazarPartido(Partido partido) {
+        // TODO: Implementar la lógica para rechazar el partido
+    }
+}
---
+++ modelos/EnJuego.java
@@ -0,0 +1,19 @@
+package modelos;
+
+
+public class EnJuego implements IEstadoPartido {
+    @Override
+    public void validarCondicions(Partido partido) {
+        // TODO: Implementar la lógica para validar las condiciones del partido en juego
+    }
+
+    @Override
+    public void aceptarPartido(Partido partido) {
+        // TODO: Implementar la lógica para aceptar el partido
+    }
+
+    @Override
+    public void rechazarPartido(Partido partido) {
+        // TODO: Implementar la lógica para rechazar el partido
+    }
+}
